067f427b504a8a6ae6567f5a8a2f77d77ba62cb7
yelena0000 <kirillnek07081999@gmail.com>
2024-12-05 10:39:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import requests
 
 
-def get_weather(city):
-    url_template = 'https://wttr.in/{}?M&n&q&T&lang=ru'
-    url_city = url_template.format(city)
-    response_city = requests.get(url_city)
-    response_city.raise_for_status()
-    return response_city.text
+def get_weather(place):
+    params = {
+        'M': '',
+        'n': '',
+        'q': '',
+        'T': '',
+        'lang': 'ru'
+    }
+    url = f'https://wttr.in/{place}'
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.text
 
 
 def main():
-    print(
-        get_weather('Лондон'),
-        get_weather('Шереметьево'),
-        get_weather('Череповец')
-    )
+    cities = [
+        'Лондон',
+        'Шереметьево',
+        'Череповец'
+    ]
+    for city in cities:
+        print(get_weather(city))
 
 
 if __name__ == '__main__':
